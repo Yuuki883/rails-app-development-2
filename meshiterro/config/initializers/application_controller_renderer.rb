196a40c25a25316afa0feb861e82809c7b1e873f
saitou yuuki <y.s@saitounoMacBook-Pro.local>
2020-01-11 00:15:59 +0900

    [Finish] 5-6

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e750684685411748900e1f3aa8f603a50bc43d42f1c05a01f966da04ff1cfffa3aed3a429b1eb0e9cccfad0d993bb98f9258300114720231020d55023fa04b7e'
