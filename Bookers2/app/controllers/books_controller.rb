ded8c9af1761806b631d4f8347fa830f137d406b
saitou yuuki <y.s@saitounoMacBook-Pro.local>
2020-01-11 00:11:30 +0900

    [Finish] 5-8

---
+++ Bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,66 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!,except: [:top]
+  before_action :correct_user, only: [:edit, :update]
+
+  def top
+  end
+
+
+  def index
+  	  @book = Book.new
+  	  @books = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+    @booknew = Book.new
+  end
+
+  def create
+  	  @book = Book.new(book_params)
+      @book.user_id = current_user.id
+  	if @book.save
+  		flash[:notice] = "You have creatad book successfully."
+        redirect_to book_path(@book)
+   else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+
+  def update
+  	@book = Book.find(params[:id])
+  		@book.update(book_params)
+   if @book.save
+  		flash[:notice] = "Book was successfully updated."
+    	redirect_to book_path(@book.id)
+  else
+    render 'edit'
+  end
+end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+  	redirect_to books_path
+  end
+
+  private
+  def book_params
+  params.require(:book).permit(:title, :body)
+ end
+
+
+ def correct_user
+  book = Book.find(params[:id])
+  if current_user.id != book.user.id
+    redirect_to books_path
+  end
+  end
+end
---
+++ Bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '00efe9c7639fed89ef9bde15d39ac05ad11a4dd06abc9076080e53b746c7dd87b11673ee48726910c0ad592120f94ff40529c40fb5e757e7123e6e067f6d1feb'
